cb3ce86e96954f22789b4b76f432a299577abcc5
abhinava31 <abhinava@hornet.technology>
2024-08-22 13:42:10 +0530

    change in regex

--- utils/validation.go
+++ utils/validation.go
@@ -6,10 +6,7 @@ import (
 )
 
 func IsOnion(identifier string) bool {
-	// TODO: At some point we will want to support i2p
-	if len(identifier) >= 22 && strings.HasSuffix(identifier, ".onion") {
-		matched, _ := regexp.MatchString(`(^|\.)[a-z2-7]{56}\.onion$`, identifier)
-		return matched
-	}
-	return false
+	// Updated regex to handle both 16 and 56 character onion addresses.
+	matched, _ := regexp.MatchString(`(^|\.)[a-z2-7]{16,56}\.onion($|\/)`, strings.ToLower(identifier))
+	return matched
 }
